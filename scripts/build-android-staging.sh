8120258c2f01a923620a8e0ad0e4bb0b5d3e6b99
Andrew Drach <andrew@solwey.com>
2023-01-18 09:00:10 -0500

    chore: add build scripts

---
+++ scripts/build-android-staging.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+# Build the app
+echo "[nasa-iss.staging]: Building Android version..."
+# ./scripts/config-env.sh staging
+yarn install
+yarn update-build
+npx jetifier
+
+cd android
+./gradlew --no-daemon clean assembleRelease
+
+# Upload to Firebase App Distribution
+APK_PATH="./app/build/outputs/apk/release/app-release.apk"
+GOOGLE_APP_ID=$(node -pe "require('./app/google-services.json').client[0].client_info.mobilesdk_app_id")
+firebase appdistribution:distribute $APK_PATH --app $GOOGLE_APP_ID --groups "android-testers"
+cd ..
+
+# Send Slack message
+# SLACK_TITLE="Android" ./scripts/send-slack-msg.sh
---
+++ scripts/build-ios-staging.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -eo pipefail
+
+switchToSystemRuby() {
+  if hash rvm 2>/dev/null; then
+    echo "Switching to system Ruby..."
+    source $HOME/.rvm/scripts/rvm
+    rvm use system
+  else
+    echo "RVM not found, skipping RVM reconfig"
+  fi
+}
+
+# Build the app
+echo "[nasa-iss.staging]: Building iOS version..."
+# ./scripts/config-env.sh staging
+
+yarn run update-build
+switchToSystemRuby
+yarn
+
+cd ios
+pod install
+export RCT_NO_LAUNCH_PACKAGER=1
+xcodebuild -workspace NasaIssApp.xcworkspace -scheme "NasaIssApp" clean
+xcodebuild -workspace NasaIssApp.xcworkspace -scheme "NasaIssApp" -configuration Release -destination generic/platform=iOS -archivePath ./NasaIssApp.xcarchive  archive | xcpretty
+rm -Rf IPA
+mkdir IPA
+xcodebuild -exportArchive -archivePath ./NasaIssApp.xcarchive -exportPath ./IPA -exportOptionsPlist ../scripts/exportOptions.plist
+
+# Upload to Firebase App Distribution
+IPA_PATH="./IPA/NasaIssApp.ipa"
+GOOGLE_APP_ID=$(defaults read $(pwd)/GoogleService-Info GOOGLE_APP_ID)
+firebase appdistribution:distribute $IPA_PATH --app $GOOGLE_APP_ID --groups "ios-testers"
+cd ..
+
+# Send Slack message
+# SLACK_TITLE="iOS" ./scripts/send-slack-msg.sh
