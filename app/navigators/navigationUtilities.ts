d221174b586eb8d02d8beba59cc1c5e8a8ec267e
Andrew Drach <andrew@solwey.com>
2025-09-05 11:03:51 +0300

    fix: android crash on app close; upgrade version to 1.7.0 (2025090617)

--- app/navigators/navigationUtilities.ts
+++ app/navigators/navigationUtilities.ts
@@ -75,12 +75,10 @@ export function useBackButtonHandler(canExit: (routeName: string) => boolean) {
     }
 
     // Subscribe when we come to life
-    BackHandler.addEventListener("hardwareBackPress", onBackPress)
+    const subscription = BackHandler.addEventListener("hardwareBackPress", onBackPress)
 
     // Unsubscribe when we're done
-    // @ts-ignore
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-return
-    return () => BackHandler.removeEventListener("hardwareBackPress", onBackPress)
+    return () => subscription.remove()
   }, [])
 }
 
