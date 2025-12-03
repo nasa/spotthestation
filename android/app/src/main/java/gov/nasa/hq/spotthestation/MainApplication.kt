3d3441e3f4085d1b5db5b21d2038dec9d5412c40
Andrew Drach <andrew@solwey.com>
2025-12-03 10:18:57 +0200

    fix: remove leftover import

--- android/app/src/main/java/gov/nasa/hq/spotthestation/MainApplication.kt
+++ android/app/src/main/java/gov/nasa/hq/spotthestation/MainApplication.kt
@@ -11,7 +11,6 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
-import org.wonday.orientation.OrientationActivityLifecycle
 import com.microsoft.codepush.react.CodePush
 import android.content.res.Configuration
 import expo.modules.ApplicationLifecycleDispatcher
